f355dbb0778855246855c851fa0a2061c428d71c
miikajuhala <miika.m.juhala@gmail.com>
2022-11-25 14:05:49 +0200

    better check for vote increment and comments

--- WebApplication1/Controllers/PollController.cs
+++ WebApplication1/Controllers/PollController.cs
@@ -21,7 +21,6 @@ namespace WebApplication1.Controllers
             _pollContext = pollContext;
         }
 
-        
 
         // GET all polls including its voteoptions
         [HttpGet]
@@ -29,10 +28,11 @@ namespace WebApplication1.Controllers
         {
             try
             {
-                //return all polls including voteoptions
+               //return all polls including voteoptions
                var all = _pollContext.Polls
                     .Include(poll => poll.VoteOptions)
                     .ToList();
+
                 if(all.Any())
                 {
                     return Ok(all);
@@ -63,14 +63,14 @@ namespace WebApplication1.Controllers
                     return NotFound("Server: No polls with id: " + id);
                 }
 
-                //if ok return poll including polls voteoptions by PollId
+                //get poll including poll's voteoptions by PollId
                 var pollWithOptions =
                     _pollContext.Polls.Where(s => s.Id == id).
                         Include(
                             poll => poll.VoteOptions
                             .Where(VoteOption => VoteOption.PollId == id))
                             .ToList();
-
+                //return poll with its options
                 return Ok(pollWithOptions);
             }
             //catch error and return to client
@@ -81,59 +81,59 @@ namespace WebApplication1.Controllers
             }
         }
 
-        // POST api/Poll
-        //POST object Poll including its VoteOptions as a list and save to database using Entity Framework
+        
+        //POST entity Poll including its VoteOptions as a list and save to database using Entity Framework
         [HttpPost]
-        public IActionResult Post([FromBody] Poll value)
+        public IActionResult Post([FromBody] Poll poll)
         {
     
             //Model exeptions are handled by Entity framework, [Required]
             try
             {
-                //add Poll called value to PollContext 
-                _pollContext.Polls.Add(value);
+                //add Poll value to PollContext 
+                _pollContext.Polls.Add(poll);
                 //and save to db
                 _pollContext.SaveChanges();
                 //return ok if no errors occurred
-                return Ok(value);
+                return Ok(poll);
             }
-            catch
+            catch (Exception ex)
             {
-                return BadRequest("Internal server error");
+                return BadRequest(ex.Message);
             }
         }
 
-        // PUT one vote to voteOption by voteOption id
-        [HttpPut("putvote/{id}")]
-        public IActionResult Put(int id)
+        // PUT one vote to voteOption by voteOption.id where pollId==VoteOption.pollId
+        [HttpPut("{pollId}/vote/{voteId}")]
+        public IActionResult Put(int pollId, int voteId)
         {
             try
             {
-                //Get right voteoption from db
-                VoteOption voteOpt = _pollContext.VoteOptions.FirstOrDefault(s => s.Id == id);
-                //increment voteoptions vote counter by one vote
+
+                //Get right voteoption from db and make sure it exists in given poll
+                VoteOption voteOpt = _pollContext.VoteOptions.FirstOrDefault(s => s.Id == voteId && s.PollId == pollId);
+                
+                //if voteoption not found from inside given poll id, return 400 bad request
                 if (voteOpt == null)
                 {
-                    return NotFound("Server error: voteoption not found");
+                    return BadRequest("Server error: voteoption not found");
                 }
-                //add one vote to voteamount
+
+                //increment voteoptions vote counter by one vote
                 voteOpt.VoteAmount++;
 
                 //save new vote amount to db
                 _pollContext.SaveChanges();
 
+                //return ok if no errors occurred
                 return Ok(voteOpt);
             }
-            catch 
+            catch (Exception ex)
             {
-                return BadRequest("Internal server error");
+                return BadRequest(ex.Message);
             }
         }
 
-        // DELETE api/<ValuesController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+
     }
 }
